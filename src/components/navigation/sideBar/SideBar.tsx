284748de833575057f1302c8be1a60ce0a18601e
Dinyar <dinyar.dev@gmail.com>
2024-06-03 12:15:42 -0400

    M44-10 customize saidebar list items, just a tryout

--- src/components/navigation/sideBar/SideBar.tsx
+++ src/components/navigation/sideBar/SideBar.tsx
@@ -34,22 +34,18 @@ const SideBar = () => {
   const drawer = (
     <div>
       <Toolbar />
-      <Divider />
+
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+        {[
+          'Dashboard',
+          'Resume',
+          'Cover Letters',
+          'Job Descriptions',
+          'Calendar',
+          'Todos',
+          'Auto Pilot',
+          'Interview Now',
+        ].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
