476720c413b1b83a52e3287e6e0fe9285d36e910
Dinyar <dinyar.dev@gmail.com>
2024-06-04 17:38:50 -0400

    M44-12 move account menu related logic into its own component

--- src/components/navigation/navbar/Navbar.tsx
+++ src/components/navigation/navbar/Navbar.tsx
@@ -5,17 +5,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../../state/store';
 import { toggleDrawer } from '../../../state/navigation/sidebarSlice';
 import Logo from '../../logo/Logo';
-import {
-  Avatar,
-  Box,
-  Divider,
-  ListItemIcon,
-  Menu,
-  MenuItem,
-  Tooltip,
-} from '@mui/material';
+import { Avatar, Box, Tooltip } from '@mui/material';
 import React from 'react';
-import { Logout, PersonAdd, Settings } from '@mui/icons-material';
+import AccountMenu from './AccountMenu';
 
 const Navbar = () => {
   const dispatch = useDispatch();
@@ -67,67 +59,11 @@ const Navbar = () => {
             <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
           </IconButton>
         </Tooltip>
-        <Menu
-          anchorEl={anchorEl}
-          id="account-menu"
+        <AccountMenu
           open={open}
-          onClose={handleClose}
-          onClick={handleClose}
-          PaperProps={{
-            elevation: 0,
-            sx: {
-              overflow: 'visible',
-              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-              mt: 1.5,
-              '& .MuiAvatar-root': {
-                width: 32,
-                height: 32,
-                ml: -0.5,
-                mr: 1,
-              },
-              '&::before': {
-                content: '""',
-                display: 'block',
-                position: 'absolute',
-                top: 0,
-                right: 14,
-                width: 10,
-                height: 10,
-                bgcolor: 'background.paper',
-                transform: 'translateY(-50%) rotate(45deg)',
-                zIndex: 0,
-              },
-            },
-          }}
-          transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-        >
-          <MenuItem onClick={handleClose}>
-            <Avatar /> Profile
-          </MenuItem>
-          <MenuItem onClick={handleClose}>
-            <Avatar /> My account
-          </MenuItem>
-          <Divider />
-          <MenuItem onClick={handleClose}>
-            <ListItemIcon>
-              <PersonAdd fontSize="small" />
-            </ListItemIcon>
-            Add another account
-          </MenuItem>
-          <MenuItem onClick={handleClose}>
-            <ListItemIcon>
-              <Settings fontSize="small" />
-            </ListItemIcon>
-            Settings
-          </MenuItem>
-          <MenuItem onClick={handleClose}>
-            <ListItemIcon>
-              <Logout fontSize="small" />
-            </ListItemIcon>
-            Logout
-          </MenuItem>
-        </Menu>
+          handleClose={handleClose}
+          anchorEl={anchorEl}
+        />
       </Toolbar>
     </AppBar>
   );
