151211cd038ffad961b9d3367f81c988a14a088b
Dinyar <dinyar.dev@gmail.com>
2024-06-04 19:18:42 -0400

    M44-13 add background to baselayer

--- src/components/baseLayer/BaseLayer.tsx
+++ src/components/baseLayer/BaseLayer.tsx
@@ -1,10 +1,15 @@
 import './baselayer.scss';
+import resume from './baseLayerImages/resume.png';
 
 interface Props {
   children: React.ReactNode;
 }
 
 const BaseLayer = ({ children }: Props) => {
+  document.documentElement.style.setProperty(
+    '--background-image-url',
+    `url(${resume})`
+  );
   return (
     <div className="pageContainer">
       <div className="left">{children}</div>
