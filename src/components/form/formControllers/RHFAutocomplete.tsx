d323973b561e197dd4fb6597d321de500ab1bb44
Dinyar <dinyar.dev@gmail.com>
2024-06-05 13:01:58 -0400

    M44-19 MUI Autocomple component requires options prop which RHFAutocomplete wrapper component has no idea about, so it needs to be passed down from the form itself, so here how we update the Props of this wrapper to accomodate that

--- src/components/form/formControllers/RHFAutocomplete.tsx
+++ src/components/form/formControllers/RHFAutocomplete.tsx
@@ -1,19 +1,24 @@
 import { Autocomplete } from '@mui/material';
 import { Controller, FieldValues, Path, useFormContext } from 'react-hook-form';
+import { AutocompleteOptions } from '../types/autocompleteOptions';
 
 //Ive been using interfaces for props in other places and typecript is telling me to stay consistent and use type instead, however this lesson uses types I will use them and ignore the error for now
 // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 type Props<T extends FieldValues> = {
   name: Path<T>;
+  options: AutocompleteOptions[];
 };
 
-export function RHFAutocomplete<T extends FieldValues>({ name }: Props<T>) {
+export function RHFAutocomplete<T extends FieldValues>({
+  name,
+  options,
+}: Props<T>) {
   const { control } = useFormContext();
   return (
     <Controller
       control={control}
       name={name}
-      render={({ params }) => <Autocomplete />}
+      render={({ params }) => <Autocomplete options={options} />}
     />
   );
 }
---
+++ src/components/form/types/autocompleteOptions.ts
@@ -0,0 +1,3 @@
+//Similary to prop definitions in RHFAutocomplete.tsx, I am ignoring this error.
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+export type AutocompleteOptions = { id: string; label: string };
