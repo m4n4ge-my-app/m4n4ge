4c932554f442333f16488156dea8a21ac63efd6f
Dinyar <dinyar.dev@gmail.com>
2024-06-05 10:12:10 -0400

    M44-19 cleanup to implement new form with MUI

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,30 +1,7 @@
 import { useForm } from 'react-hook-form';
 
 export function Users() {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<{ email: string }>({ mode: 'all' });
+  const { register } = useForm({ mode: 'all' });
 
-  const onsubmit = () => {
-    console.log('submitted');
-  };
-
-  return (
-    <form onSubmit={handleSubmit(onsubmit)}>
-      <input
-        {...register('email', {
-          required: { value: true, message: 'Email is required' },
-          pattern: {
-            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-            message: 'Invalid email address',
-          },
-          maxLength: { value: 10, message: 'Email is too long' },
-        })}
-        placeholder="email"
-      />
-      <p>{errors.email?.message}</p>
-    </form>
-  );
+  return <></>;
 }
