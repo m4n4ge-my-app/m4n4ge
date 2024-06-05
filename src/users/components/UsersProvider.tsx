1002406a87a563267bcdcf2e30d557923fcf58c8
Dinyar <dinyar.dev@gmail.com>
2024-06-05 15:17:24 -0400

    M44-19 add DevTool from @hookform/devtools, this is just panel that apears with the application that shows up state of the form fields

--- src/users/components/UsersProvider.tsx
+++ src/users/components/UsersProvider.tsx
@@ -2,6 +2,7 @@ import { FormProvider, useForm } from 'react-hook-form';
 import { Users } from './Users';
 import { Schema, defaultValues, schema } from '../types/schema';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { DevTool } from '@hookform/devtools';
 
 export function UsersProvider() {
   const methods = useForm<Schema>({
@@ -13,6 +14,7 @@ export function UsersProvider() {
   return (
     <FormProvider {...methods}>
       <Users />
+      <DevTool control={methods.control} />
     </FormProvider>
   );
 }
