ff50672c5709b96cc6089a5d22b4aa9e061b7d7e
Dinyar <dinyar.dev@gmail.com>
2024-06-05 09:46:57 -0400

    M44-19 by default Zod handles error massages but we can override the error messages like this

--- src/users/types/schema.ts
+++ src/users/types/schema.ts
@@ -1,5 +1,9 @@
 import { z } from 'zod';
 
 export const schema = z.object({
-  name: z.string().min(2).max(255),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, { message: 'Required, also too short' })
+    .max(100, { message: 'Too long' }),
 });
