dbb98cad6360b8cf179dccc81e6db7a741b61f04
Dinyar <dinyar.dev@gmail.com>
2024-06-05 10:33:25 -0400

    M44-19 MUI TextFiled has a boolean error prop that change the ui of the field depending on the state of this error prop. we can drive this value from errors from react-hook-form formstate and convert it to boolean value by using the typescript !!

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -4,14 +4,17 @@ import { Schema, schema } from '../types/schema';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 export function Users() {
-  const { register } = useForm<Schema>({
+  const {
+    register,
+    formState: { errors },
+  } = useForm<Schema>({
     mode: 'all',
     resolver: zodResolver(schema),
   });
 
   return (
     <Stack sx={{ gap: 2 }}>
-      <TextField {...register('name')} label="Name" />
+      <TextField {...register('name')} label="Name" error={!!errors.name} />
       <TextField {...register('email')} label="Email" />
     </Stack>
   );
