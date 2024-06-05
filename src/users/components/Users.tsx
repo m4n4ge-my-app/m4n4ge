f9e9e19bae1a2fc8f5c486617f976f0e502e1d61
Dinyar <dinyar.dev@gmail.com>
2024-06-05 09:19:42 -0400

    M44-19 this is how we can show errors to users

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,18 +1,24 @@
 import { useForm } from 'react-hook-form';
 
 export function Users() {
-  const { register } = useForm<{ email: string }>();
+  const {
+    register,
+    formState: { errors },
+  } = useForm<{ email: string }>();
   return (
-    <input
-      {...register('email', {
-        required: { value: true, message: 'Email is required' },
-        pattern: {
-          value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-          message: 'Invalid email address',
-        },
-        maxLength: { value: 100, message: 'Email is too long' },
-      })}
-      placeholder="email"
-    />
+    <>
+      <input
+        {...register('email', {
+          required: { value: true, message: 'Email is required' },
+          pattern: {
+            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+            message: 'Invalid email address',
+          },
+          maxLength: { value: 100, message: 'Email is too long' },
+        })}
+        placeholder="email"
+      />
+      <p>{errors.email?.message}</p>
+    </>
   );
 }
