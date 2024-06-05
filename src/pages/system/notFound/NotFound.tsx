e974abc0e78a232212934dee39a02cacf32235d6
Dinyar <dinyar.dev@gmail.com>
2024-06-04 20:29:01 -0400

    M44-13 implement NotFound page

--- src/pages/system/notFound/NotFound.tsx
+++ src/pages/system/notFound/NotFound.tsx
@@ -1,9 +1,37 @@
+import BaseLayer from '../../../components/baseLayer/BaseLayer';
+import NotFoundGrid from '../../../components/grids/NotFoundGrid';
+import notFoundImage from '../../../components/baseLayer/baseLayerImages/404.png';
+
 interface NotFoundProps {
   isPrivateRoute: boolean;
 }
 
 const NotFound = ({ isPrivateRoute }: NotFoundProps) => {
-  return <div>NotFound</div>;
+  return isPrivateRoute ? (
+    // TODO: temporarly using resume background image for now, will replace it with appropriate image
+    <BaseLayer type="resume">
+      <NotFoundGrid />
+    </BaseLayer>
+  ) : (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 10,
+        height: '100vh',
+      }}
+    >
+      {/* TODO: further refind this page */}
+      Not Found page: under development
+      <img
+        src={notFoundImage}
+        alt="image"
+        style={{ width: 300, height: 300 }}
+      />
+    </div>
+  );
 };
 
 export default NotFound;
