5b604702b0f6a7f75d2134262d37d4c3381c149d
Dinyar <dinyar.dev@gmail.com>
2024-06-06 18:35:30 -0400

    M44-19 update RHFToggleButtonGroup

--- src/components/form/formControllers/RHFToggleButtonGroup.tsx
+++ src/components/form/formControllers/RHFToggleButtonGroup.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Controller, FieldValues, Path, useFormContext } from 'react-hook-form';
 import { Options } from '../types/Options';
-import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { FormHelperText, ToggleButton, ToggleButtonGroup } from '@mui/material';
 
 type Props<T extends FieldValues> = {
   name: Path<T>;
@@ -19,22 +19,29 @@ export function RHFToggleButtonGroup<T extends FieldValues>({
     <Controller
       control={control}
       name={name}
-      render={({ field: { onChange, value, ...restField } }) => (
-        <ToggleButtonGroup
-          onChange={(_, newValue) => {
-            if (newValue.length) {
-              onChange(newValue);
-            }
-          }}
-          value={value.length ? value : [options?.[0].id]}
-          {...restField}
-        >
-          {options?.map((option) => (
-            <ToggleButton value={option.id} key={option.id}>
-              {option.label}
-            </ToggleButton>
-          ))}
-        </ToggleButtonGroup>
+      render={({
+        field: { onChange, value, ...restField },
+        fieldState: { error },
+      }) => (
+        <>
+          <ToggleButtonGroup
+            onChange={(_, newValue) => {
+              if (newValue.length) {
+                onChange(newValue);
+              }
+            }}
+            value={value.length ? value : [options?.[0].id]}
+            {...restField}
+            fullWidth
+          >
+            {options?.map((option) => (
+              <ToggleButton value={option.id} key={option.id} size="small">
+                {option.label}
+              </ToggleButton>
+            ))}
+          </ToggleButtonGroup>
+          <FormHelperText>{error?.message}</FormHelperText>
+        </>
       )}
     ></Controller>
   );
