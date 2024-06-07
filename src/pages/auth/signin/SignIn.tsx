369e4d3cb2b78244079a668974bd2ff8583eccb2
Dinyar <dinyar.dev@gmail.com>
2024-06-07 19:05:55 -0400

    M44-15 add a welcome text

--- src/pages/auth/signin/SignIn.tsx
+++ src/pages/auth/signin/SignIn.tsx
@@ -1,3 +1,4 @@
+import { Typography } from '@mui/material';
 import TextLogo from '../../../components/logo/TextLogo';
 import './signin.scss';
 
@@ -6,6 +7,13 @@ const SignIn = () => {
     <div className="signinContainer">
       <div className="logo">
         <TextLogo />
+        <Typography
+          variant="h4"
+          className="welcomeText"
+          style={{ color: '#407BFF', fontWeight: 'bold', marginBottom: '20px' }}
+        >
+          Welcome back! Your journey awaits, log in to continue.
+        </Typography>
       </div>
     </div>
   );
