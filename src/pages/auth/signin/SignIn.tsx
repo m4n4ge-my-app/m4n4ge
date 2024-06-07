f02d0e6968d1e83f4ffebefd997a1dce400e4d32
Dinyar <dinyar.dev@gmail.com>
2024-06-07 19:00:46 -0400

    M44-15 add the logo in

--- src/pages/auth/signin/SignIn.tsx
+++ src/pages/auth/signin/SignIn.tsx
@@ -1,7 +1,14 @@
+import TextLogo from '../../../components/logo/TextLogo';
 import './signin.scss';
 
 const SignIn = () => {
-  return <div className="signinContainer">SignIn</div>;
+  return (
+    <div className="signinContainer">
+      <div className="logo">
+        <TextLogo />
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
