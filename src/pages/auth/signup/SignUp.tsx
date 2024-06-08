188b0ea694af90c3f393fefd690b62e0af93f1be
Dinyar <dinyar.dev@gmail.com>
2024-06-08 13:55:48 -0400

    M44-15 apply AuthGrid to SignUp page

--- src/pages/auth/signup/SignUp.tsx
+++ src/pages/auth/signup/SignUp.tsx
@@ -1,5 +1,13 @@
+import AuthGrid from '../../../components/grids/auth/AuthGrid';
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <AuthGrid
+      messageTitle="Start your adventure with us!"
+      message="Register to create a new account"
+      form={<h1>Sign Up Page</h1>}
+    />
+  );
 };
 
 export default SignUp;
