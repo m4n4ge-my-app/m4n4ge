b8905d4832a5c70d4a9338b0c838c377536817e0
Dinyar <dinyar.dev@gmail.com>
2024-06-08 13:31:33 -0400

    M44-15 move grid into its own componet so it can be used for register page

---
+++ src/components/grids/auth/AuthGrid.tsx
@@ -0,0 +1,59 @@
+import { Grid, Box, Stack, Typography } from '@mui/material';
+import TextLogo from '../../../components/logo/TextLogo';
+import React from 'react';
+import './authgrid.scss';
+
+interface Props {
+  messageTitle: string;
+  message: string;
+  form: React.ReactNode;
+}
+
+const AuthGrid = ({ messageTitle, message, form }: Props) => {
+  return (
+    <Grid container className="signinContainer">
+      <Grid item xs={12} className="header">
+        <Stack className="logo">
+          <TextLogo />
+        </Stack>
+      </Grid>
+      <Grid container item className="lowerSection">
+        <Box
+          component={Grid}
+          item
+          md={6}
+          className="leftSide"
+          display={{ sm: 'none', md: 'block' }}
+        >
+          <Box className="welcomeText">
+            <Typography
+              variant="h4"
+              style={{
+                color: '#264a99',
+                fontWeight: 'bold',
+                marginBottom: '20px',
+              }}
+            >
+              {messageTitle}
+            </Typography>
+            <Typography
+              variant="h5"
+              style={{
+                color: '#6695ff',
+                fontWeight: 'normal',
+                marginBottom: '20px',
+              }}
+            >
+              {message}
+            </Typography>
+          </Box>
+        </Box>
+        <Grid item sm={12} md={6} className="rightSide">
+          {form}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default AuthGrid;
--- src/pages/auth/signin/SignIn.tsx
+++ src/pages/auth/signin/SignIn.tsx
@@ -1,51 +1,13 @@
-import { Grid, Box, Stack, Typography } from '@mui/material';
-import TextLogo from '../../../components/logo/TextLogo';
+import AuthGrid from '../../../components/grids/auth/AuthGrid';
 import './signin.scss';
 
 const SignIn = () => {
   return (
-    <Grid container className="signinContainer">
-      <Grid item xs={12} className="header">
-        <Stack className="logo">
-          <TextLogo />
-        </Stack>
-      </Grid>
-      <Grid container item className="lowerSection">
-        <Box
-          component={Grid}
-          item
-          md={6}
-          className="leftSide"
-          display={{ sm: 'none', md: 'block' }}
-        >
-          <Box className="welcomeText">
-            <Typography
-              variant="h4"
-              style={{
-                color: '#264a99',
-                fontWeight: 'bold',
-                marginBottom: '20px',
-              }}
-            >
-              Welcome back!
-            </Typography>
-            <Typography
-              variant="h5"
-              style={{
-                color: '#6695ff',
-                fontWeight: 'normal',
-                marginBottom: '20px',
-              }}
-            >
-              Your journey awaits, log in to continue.
-            </Typography>
-          </Box>
-        </Box>
-        <Grid item sm={12} md={6} className="rightSide">
-          This is signin form area
-        </Grid>
-      </Grid>
-    </Grid>
+    <AuthGrid
+      messageTitle="Welcome Back!"
+      message="Your journey awaits, log in to continue."
+      form={<div>Sign In Form</div>}
+    />
   );
 };
 
