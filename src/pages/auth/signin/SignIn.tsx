00c2b30eaced3f77a6ad3d5e8e919d3a035a9cd5
Dinyar <dinyar.dev@gmail.com>
2024-06-08 12:45:14 -0400

    m44-15 use mui Grids to have responsive layout

--- src/pages/auth/signin/SignIn.tsx
+++ src/pages/auth/signin/SignIn.tsx
@@ -1,21 +1,30 @@
-import { Typography } from '@mui/material';
+import { Grid, Box, Stack } from '@mui/material';
 import TextLogo from '../../../components/logo/TextLogo';
 import './signin.scss';
 
 const SignIn = () => {
   return (
-    <div className="signinContainer">
-      <div className="logo">
-        <TextLogo />
-        <Typography
-          variant="h4"
-          className="welcomeText"
-          style={{ color: '#407BFF', fontWeight: 'bold', marginBottom: '20px' }}
+    <Grid container className="signinContainer">
+      <Grid item xs={12} className="header">
+        <Stack className="logo">
+          <TextLogo />
+        </Stack>
+      </Grid>
+      <Grid container item className="lowerSection">
+        <Box
+          component={Grid}
+          item
+          sm={6}
+          className="leftSide"
+          display={{ xs: 'none', sm: 'block' }}
         >
-          Welcome back! Your journey awaits, log in to continue.
-        </Typography>
-      </div>
-    </div>
+          Welcome Text
+        </Box>
+        <Grid item xs={12} sm={6} className="rightSide">
+          This is signin form area
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
