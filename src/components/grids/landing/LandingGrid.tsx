66e795cdcc97e578171cae610c0c4f34f7bc6b14
Dinyar <dinyar.dev@gmail.com>
2024-06-08 22:15:06 -0400

    M44-15 add mui tab to show diffrent app features in landing page

--- src/components/grids/landing/LandingGrid.tsx
+++ src/components/grids/landing/LandingGrid.tsx
@@ -1,10 +1,16 @@
-import { Box, Button, Grid } from '@mui/material';
+import { Box, Button, Grid, Tab, Tabs, Typography } from '@mui/material';
 import TextLogo from '../../logo/TextLogo';
 import { Link } from 'react-router-dom';
 import './landing.scss';
 import Logo from '../../logo/Logo';
+import { useState } from 'react';
 
 const LandingGrid = () => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <Grid container className="landingContainer">
       <Grid
@@ -40,7 +46,52 @@ const LandingGrid = () => {
           </Box>
         </Grid>
       </Grid>
-      <Grid container item className="bodySection"></Grid>
+      <Grid container item className="bodySection">
+        <Grid
+          container
+          item
+          xs={12}
+          className="bodySection"
+          justifyContent="center"
+          direction="column"
+          sx={{ padding: '20px', flexGrow: 1, minHeight: '100vh' }}
+        >
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            centered
+            sx={{
+              '& .MuiTabs-indicator': {
+                width: 'fit-content',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+              },
+            }}
+          >
+            {[...Array(5)].map((_, i) => (
+              <Tab key={i} label={`Tab ${i + 1}`} />
+            ))}
+          </Tabs>
+          {[...Array(5)].map((_, i) => (
+            <Box key={i} display={value === i ? 'block' : 'none'}>
+              <Grid container>
+                <Grid item xs={6}>
+                  <Typography variant="h4">Feature {i + 1}</Typography>
+                  <Typography variant="body1">
+                    Details about feature {i + 1}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  {/* Placeholder for image */}
+                  <Box
+                    sx={{ height: 200, width: '100%', bgcolor: 'grey.300' }}
+                  />
+                </Grid>
+              </Grid>
+            </Box>
+          ))}
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
