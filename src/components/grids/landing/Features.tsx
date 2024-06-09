5110fcd7d515d011980b5ce9cd6c06c97bd278f3
Dinyar <dinyar.dev@gmail.com>
2024-06-09 02:04:15 -0400

    M44-17 move features into its own component

---
+++ src/components/grids/landing/Features.tsx
@@ -0,0 +1,156 @@
+import { Box, Button, Grid, Stack, Tab, Tabs, Typography } from '@mui/material';
+import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { appFeatures } from './appFeatures';
+import theme from '../../../theme';
+import './landing.scss';
+
+const Features = () => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <>
+      <Stack justifyContent="center" alignItems="center" sx={{ width: '100%' }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          centered
+          sx={{
+            '& .MuiTabs-indicator': {
+              width: 'fit-content',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+            },
+            position: 'sticky',
+            top: 0,
+          }}
+        >
+          {appFeatures.map((feature) => (
+            <Tab key={feature.title} label={feature.title} />
+          ))}
+        </Tabs>
+      </Stack>
+      <Grid container item className="bodySection">
+        <Grid
+          container
+          item
+          xs={12}
+          direction="column"
+          sx={{
+            padding: '20px',
+            flexGrow: 1,
+            minHeight: '100vh',
+            height: 'auto',
+          }}
+        >
+          {appFeatures.map((feature, i) => (
+            <Box key={i} display={value === i ? 'block' : 'none'}>
+              <Grid container>
+                <Grid
+                  item
+                  xs={6}
+                  style={{ padding: '20px', paddingLeft: '100px' }}
+                >
+                  <Typography
+                    variant="h5"
+                    style={{
+                      color: '#264a99',
+                      fontWeight: 'bold',
+                      marginBottom: '20px',
+                      fontSize: '1.5em',
+                    }}
+                  >
+                    {feature.message}
+                  </Typography>
+                  <List sx={{ marginBottom: '20px' }}>
+                    {feature.description.map((point, index) => (
+                      <ListItem key={index} alignItems="flex-start">
+                        <ListItemIcon>
+                          <TaskAltIcon
+                            style={{ color: theme.palette.primary.main }}
+                          />
+                        </ListItemIcon>
+                        <ListItemText
+                          primary={point}
+                          style={{
+                            color: '#6695ff',
+                            fontWeight: 'normal',
+                            marginBottom: '20px',
+                            fontSize: '1.2em',
+                          }}
+                        />
+                      </ListItem>
+                    ))}
+                  </List>
+                  <Link to="/signup">
+                    <Button variant="contained" color="primary">
+                      {"Let's Get Started!"}
+                    </Button>
+                  </Link>
+                </Grid>
+                <Grid item xs={6}>
+                  <Box
+                    sx={{
+                      height: '100%',
+                      width: '100%',
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'flex-start',
+                      paddingTop: '40px',
+                    }}
+                  >
+                    <img
+                      src={feature.image}
+                      alt={feature.title}
+                      style={{
+                        maxWidth: '100%',
+                        maxHeight: '50%',
+                        objectFit: 'contain',
+                      }}
+                    />
+                  </Box>
+                </Grid>
+              </Grid>
+            </Box>
+          ))}
+        </Grid>{' '}
+        <Grid
+          container
+          item
+          xs={12}
+          direction="column"
+          sx={{ padding: '20px', flexGrow: 1, minHeight: '100vh' }}
+        >
+          {appFeatures.map((feature, i) => (
+            <Box key={i} display={value === i ? 'block' : 'none'}>
+              <Grid container>
+                <Grid item xs={6}>
+                  <Typography variant="h4">{feature.title}</Typography>
+                  <Typography variant="body1">{feature.message}</Typography>
+                  <Typography variant="body2">{feature.description}</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Box sx={{ height: 200, width: '100%' }}>
+                    <img
+                      src={feature.image}
+                      alt={feature.title}
+                      style={{ width: '100%', height: '100%' }}
+                    />
+                  </Box>
+                </Grid>
+              </Grid>
+            </Box>
+          ))}
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Features;
--- src/components/grids/landing/LandingGrid.tsx
+++ src/components/grids/landing/LandingGrid.tsx
@@ -1,20 +1,11 @@
-import { Box, Button, Grid, Stack, Tab, Tabs, Typography } from '@mui/material';
-import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
-import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import { Box, Button, Grid } from '@mui/material';
 import TextLogo from '../../logo/TextLogo';
 import { Link } from 'react-router-dom';
 import './landing.scss';
 import Logo from '../../logo/Logo';
-import { useState } from 'react';
-import { appFeatures } from './appFeatures';
-import theme from '../../../theme';
+import Features from './Features';
 
 const LandingGrid = () => {
-  const [value, setValue] = useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
   return (
     <Grid container className="landingContainer">
       <Grid
@@ -34,7 +25,7 @@ const LandingGrid = () => {
           </Box>
         </Grid>
         <Grid item xs={6} sm={8} md={10} container justifyContent="flex-end">
-          <Box pr={{ xs: 2, sm: 3 }}>
+          <Box pr={{ xlxs: 2, sm: 3 }}>
             <Link to="/signin">
               <Button variant="outlined" color="primary">
                 Sign In
@@ -50,139 +41,7 @@ const LandingGrid = () => {
           </Box>
         </Grid>
       </Grid>
-      <Stack justifyContent="center" alignItems="center" sx={{ width: '100%' }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          centered
-          sx={{
-            '& .MuiTabs-indicator': {
-              width: 'fit-content',
-              marginLeft: 'auto',
-              marginRight: 'auto',
-            },
-            position: 'sticky',
-            top: 0,
-          }}
-        >
-          {appFeatures.map((feature) => (
-            <Tab key={feature.title} label={feature.title} />
-          ))}
-        </Tabs>
-      </Stack>
-      <Grid container item className="bodySection">
-        <Grid
-          container
-          item
-          xs={12}
-          direction="column"
-          sx={{
-            padding: '20px',
-            flexGrow: 1,
-            minHeight: '100vh',
-            height: 'auto',
-          }}
-        >
-          {appFeatures.map((feature, i) => (
-            <Box key={i} display={value === i ? 'block' : 'none'}>
-              <Grid container>
-                <Grid
-                  item
-                  xs={6}
-                  style={{ padding: '20px', paddingLeft: '100px' }}
-                >
-                  <Typography
-                    variant="h5"
-                    style={{
-                      color: '#264a99',
-                      fontWeight: 'bold',
-                      marginBottom: '20px',
-                      fontSize: '1.5em',
-                    }}
-                  >
-                    {feature.message}
-                  </Typography>
-                  <List>
-                    {feature.description.map((point, index) => (
-                      <ListItem key={index} alignItems="flex-start">
-                        <ListItemIcon>
-                          <TaskAltIcon
-                            style={{ color: theme.palette.primary.main }}
-                          />
-                        </ListItemIcon>
-                        <ListItemText
-                          primary={point}
-                          style={{
-                            color: '#6695ff',
-                            fontWeight: 'normal',
-                            marginBottom: '20px',
-                            fontSize: '1.2em',
-                          }}
-                        />
-                      </ListItem>
-                    ))}
-                  </List>
-                  <Link to="/signup">
-                    <Button variant="contained" color="primary">
-                      {"Let's Get Started!"}
-                    </Button>
-                  </Link>
-                </Grid>
-                <Grid item xs={6}>
-                  <Box
-                    sx={{
-                      height: '100%',
-                      width: '100%',
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'flex-start',
-                      paddingTop: '40px',
-                    }}
-                  >
-                    <img
-                      src={feature.image}
-                      alt={feature.title}
-                      style={{
-                        maxWidth: '100%',
-                        maxHeight: '50%',
-                        objectFit: 'contain',
-                      }}
-                    />
-                  </Box>
-                </Grid>
-              </Grid>
-            </Box>
-          ))}
-        </Grid>{' '}
-        <Grid
-          container
-          item
-          xs={12}
-          direction="column"
-          sx={{ padding: '20px', flexGrow: 1, minHeight: '100vh' }}
-        >
-          {appFeatures.map((feature, i) => (
-            <Box key={i} display={value === i ? 'block' : 'none'}>
-              <Grid container>
-                <Grid item xs={6}>
-                  <Typography variant="h4">{feature.title}</Typography>
-                  <Typography variant="body1">{feature.message}</Typography>
-                  <Typography variant="body2">{feature.description}</Typography>
-                </Grid>
-                <Grid item xs={6}>
-                  <Box sx={{ height: 200, width: '100%' }}>
-                    <img
-                      src={feature.image}
-                      alt={feature.title}
-                      style={{ width: '100%', height: '100%' }}
-                    />
-                  </Box>
-                </Grid>
-              </Grid>
-            </Box>
-          ))}
-        </Grid>
-      </Grid>
+      <Features />
     </Grid>
   );
 };
