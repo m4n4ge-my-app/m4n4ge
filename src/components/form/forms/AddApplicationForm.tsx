9b917aa84cabfed6bad0bba70a32734de3132eae
Dinyar <dinyar.dev@gmail.com>
2024-06-10 12:16:18 -0400

    M44-20 partial commit, testing new grid

--- src/components/form/forms/AddApplicationForm.tsx
+++ src/components/form/forms/AddApplicationForm.tsx
@@ -24,188 +24,56 @@ const AddApplicationForm = () => {
   }, [watch]);
 
   return (
-    <Grid container padding="20px" sx={{ position: 'relative' }}>
-      {/* Left Panel */}
-      <Grid
-        container
-        spacing={3}
-        item
-        xs={12}
-        sm={12}
-        md={12}
-        lg={6}
-        xl={6}
-        sx={{ marginTop: '20px' }}
-      >
-        {/* Employer Name */}
-        <PanelItemWrapper>
+    <Grid container padding="20px">
+      <Grid container item xs={12} sx={{ marginTop: '20px' }}>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={10}
+          lg={4}
+          sx={{ backgroundColor: 'orange' }}
+        >
           <RHFTextField<AddAppSchema>
             name="employer"
             label="Employer Name"
             size="small"
             fullWidth
           />
-        </PanelItemWrapper>
-
-        {/* Position Name */}
-        <PanelItemWrapper>
-          <RHFTextField<AddAppSchema>
-            name="position"
-            label="Position Name"
-            size="small"
-            fullWidth
-          />
-        </PanelItemWrapper>
-
-        {/* Job Location */}
-        <PanelItemWrapper>
-          <RHFTextField<AddAppSchema>
-            name="location"
-            label="Job Location"
-            size="small"
-            fullWidth
-          />
-        </PanelItemWrapper>
-
-        {/* Platform */}
-        <PanelItemWrapper>
-          <RHFSelect<AddAppSchema>
-            name="platform"
-            label="Job Platform"
-            options={[
-              { id: '1', label: 'CareerBuilder' },
-              { id: '2', label: 'Company Website' },
-              { id: '3', label: 'Direct Email' },
-              { id: '4', label: 'Dice' },
-              { id: '5', label: 'FlexJobs' },
-              { id: '6', label: 'Glassdoor' },
-              { id: '7', label: 'Indeed' },
-              { id: '8', label: 'LinkedIn' },
-              { id: '9', label: 'Monster' },
-              { id: '10', label: 'SimplyHired' },
-              { id: '11', label: 'Wellfound' },
-              { id: '12', label: 'Workopolis' },
-              { id: '13', label: 'ZipRecruiter' },
-              { id: '14', label: 'Other' },
-            ]}
-          />
-        </PanelItemWrapper>
-
-        {/* Job Application Date */}
-        <PanelItemWrapper>
-          <RHFDateCalendar<AddAppSchema> name="applicationDate" />
-        </PanelItemWrapper>
-
-        {/* Job Post Posting and Ending Datates */}
-        <PanelItemWrapper>
-          <Typography
-            fontSize={16}
-            sx={{ display: 'flex', flexDirection: 'start' }}
-          >
-            Job Post Posting & Ending Dates
-          </Typography>
-          <Box sx={{ display: 'flex', gap: 2, marginTop: '15px' }}>
-            <RHFDatePicker<AddAppSchema>
-              name="jobPostPostingDate"
-              label="Posting Date"
-            />
-            <RHFDatePicker<AddAppSchema>
-              name="jobPostExpirationDate"
-              label="Ending Date"
-            />
-          </Box>
-        </PanelItemWrapper>
-
-        {/* Work Model */}
-        <PanelItemWrapper>
-          <Typography
-            fontSize={16}
-            sx={{
-              display: 'flex',
-              flexDirection: 'start',
-              marginBottom: '10px',
-            }}
-          >
-            Work Model
-          </Typography>
-          <RHFToggleButtonGroup<AddAppSchema>
-            name="workModel"
-            options={[
-              { id: '1', label: 'On Site' },
-              { id: '2', label: 'Hybrid' },
-              { id: '3', label: 'Remote' },
-            ]}
-          />
-        </PanelItemWrapper>
-
-        {/* Invisible Div */}
-        <PanelItemWrapper>
-          <div style={{ visibility: 'hidden', height: '200px' }}>
-            {/* For some reason without this invisible div, Work Model section above becomes unclicable. */}
-          </div>
-        </PanelItemWrapper>
+        </Grid>
       </Grid>
-      {/* Right Panel */}
-      <Grid
-        item
-        xs={12}
-        sm={12}
-        md={12}
-        lg={6}
-        xl={6}
-        sx={{ marginTop: '40px' }}
-      >
-        {/* Note */}
-        <PanelItemWrapper>
-          <RHFTextArea<AddAppSchema> name="note" />
-        </PanelItemWrapper>
 
-        {/* File Upload */}
-        <PanelItemWrapper>
-          <Box
-            sx={{
-              mt: 0,
-              border: 1,
-              borderColor: 'divider',
-              height: 150,
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-              borderRadius: 1,
-              marginBottom: '20px',
-            }}
-            gap={1}
-          >
-            <Button variant="outlined" component="label">
-              Upload Job Decription
-              <input type="file" hidden />
-            </Button>
-            <Typography variant="body2">
-              Click the button to upload a file
-            </Typography>
-          </Box>
-        </PanelItemWrapper>
-        <PanelItemWrapper>
-          <RHFFavoriteCheckbox<AddAppSchema> name="isFavorite" />
-        </PanelItemWrapper>
-
-        {/* Sumbit Button */}
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            position: 'absolute',
-            right: 0,
-            bottom: 0,
-            width: '100%',
-            padding: '20px',
-          }}
+      <Grid container item xs={12}>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={10}
+          lg={4}
+          sx={{ backgroundColor: 'blue' }}
         >
-          <Button variant="contained" type="submit" size="small">
-            Add Application
-          </Button>
-        </Box>
+          postion name
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={10}
+          lg={4}
+          sx={{ backgroundColor: 'yellow' }}
+        >
+          location
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={10}
+          lg={4}
+          sx={{ backgroundColor: 'purple' }}
+        >
+          platform
+        </Grid>
       </Grid>
     </Grid>
   );
