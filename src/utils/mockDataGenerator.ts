c071cbf11b7244dae63a494e95fc7e9b9b8a4bac
Dinyar <dinyar.dev@gmail.com>
2024-06-17 17:58:31 -0400

    update data generator, match ui component props, add more data, function to group date with same date

--- src/utils/mockDataGenerator.ts
+++ src/utils/mockDataGenerator.ts
@@ -1,13 +1,13 @@
 interface Application {
-  isFavorite: boolean;
-  'Employer Name': string;
-  'Position Name': string;
-  Location: string;
-  'Application Date': string;
-  Platform: string;
-  Status: string;
-  Notes: string;
-  'Work Mode': string;
+  isFavourite: boolean;
+  employerName: string;
+  positionName: string;
+  location: string;
+  applicationDate: string;
+  platform: string;
+  status: string;
+  note: string;
+  workMode: string;
 }
 
 function getRandomDate(start: Date, end: Date): string {
@@ -30,13 +30,25 @@ function getRandomString(words: string): string {
     .join(' ');
 }
 
-const companies = ['Google', 'Microsoft', 'Apple', 'Amazon', 'Facebook'];
+const companies = [
+  'Google',
+  'Microsoft',
+  'Apple',
+  'Amazon',
+  'Facebook',
+  'Netflix',
+  'Tesla',
+  'IBM',
+];
 const positions = [
   'Software Developer',
   'Data Scientist',
   'Product Manager',
   'System Analyst',
   'Web Developer',
+  'UX Designer',
+  'Database Administrator',
+  'Network Engineer',
 ];
 const locations = [
   'London, ON',
@@ -44,8 +56,17 @@ const locations = [
   'Vancouver, BC',
   'Seattle, WA',
   'San Francisco, CA',
+  'New York, NY',
+  'Austin, TX',
+  'Chicago, IL',
 ];
 const platforms = [
+  'CareerBuilder',
+  'FlexJobs',
+  'WellFound',
+  'Glassdoor',
+  'SimplyHired',
+  'Other',
   'LinkedIn',
   'Indeed',
   'ZipRecruiter',
@@ -63,26 +84,55 @@ const statuses = [
   'Accepted',
 ];
 const workModes = ['on-site', 'hybrid', 'remote'];
-const notes =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nunc ex, commodo sit amet facilisis a, malesuada a urna';
+const notes = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
 
 export const applications: Application[] = [];
 
+const uniqueDates: string[] = [];
+for (let i = 0; i < 10; i++) {
+  uniqueDates.push(
+    getRandomDate(new Date('2023-07-12'), new Date('2024-08-01'))
+  );
+}
+
 for (let i = 0; i < 50; i++) {
   applications.push({
-    isFavorite: Math.random() < 0.5,
-    'Employer Name': companies[Math.floor(Math.random() * companies.length)],
-    'Position Name': positions[Math.floor(Math.random() * positions.length)],
-    Location: locations[Math.floor(Math.random() * locations.length)],
-    'Application Date': getRandomDate(
-      new Date('2023-07-12'),
-      new Date('2024-03-01')
-    ),
-    Platform: platforms[Math.floor(Math.random() * platforms.length)],
-    Status: statuses[Math.floor(Math.random() * statuses.length)],
-    Notes: getRandomString(notes),
-    'Work Mode': workModes[Math.floor(Math.random() * workModes.length)],
+    isFavourite: Math.random() < 0.5,
+    employerName: companies[Math.floor(Math.random() * companies.length)],
+    positionName: positions[Math.floor(Math.random() * positions.length)],
+    location: locations[Math.floor(Math.random() * locations.length)],
+    applicationDate:
+      uniqueDates[Math.floor(Math.random() * uniqueDates.length)], // Select a random date from the uniqueDates array
+    platform: platforms[Math.floor(Math.random() * platforms.length)],
+    status: statuses[Math.floor(Math.random() * statuses.length)],
+    note: getRandomString(notes),
+    workMode: workModes[Math.floor(Math.random() * workModes.length)],
   });
 }
 
 console.log(applications);
+
+function groupByDate(
+  applications: Application[]
+): Record<string, Application[]>[] {
+  const groupedApplications: Record<string, Application[]> =
+    applications.reduce(
+      (grouped, application) => {
+        const date = application.applicationDate;
+        if (!grouped[date]) {
+          grouped[date] = [];
+        }
+        grouped[date].push(application);
+        return grouped;
+      },
+      {} as Record<string, Application[]>
+    );
+
+  return Object.entries(groupedApplications).map(([date, applications]) => ({
+    [date]: applications,
+  }));
+}
+
+export const groupedApplicationsByDate = groupByDate(applications);
+
+console.log(groupedApplicationsByDate);
