6bf5c2bcd011e45776a4379de3528f1c18ba36ca
Dinyar <dinyar.dev@gmail.com>
2024-06-18 23:13:36 -0400

    add date

--- src/components/motivationBar/MotivationBar.tsx
+++ src/components/motivationBar/MotivationBar.tsx
@@ -1,11 +1,16 @@
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
+import moment from 'moment';
 
 const MotivationBar = () => {
+  const today = moment();
+  const dayOfWeek = today.format('dddd');
+  const restOfDate = today.format('MMMM D, YYYY');
+
   return (
     <Grid container>
       <Grid item xs={12} sm={4}>
-        {/* Content for the left column */}
-        <div>Left Column</div>
+        <Typography variant="h4">{dayOfWeek}</Typography>
+        <Typography variant="h6">{restOfDate}</Typography>
       </Grid>
       <Grid item xs={12} sm={8}>
         {/* Content for the right column */}
