3815c7fa468b214db7359478a1041fd2ddaf88f5
Dinyar <dinyar.dev@gmail.com>
2024-06-19 22:53:32 -0400

    remove the extra piechart, also remove the helper background color

--- src/components/widgets/applicationsOverview/ApplicationsOverview.tsx
+++ src/components/widgets/applicationsOverview/ApplicationsOverview.tsx
@@ -17,7 +17,6 @@ const ApplicationsOverview = () => {
         flexDirection: 'column',
         justifyContent: 'space-between',
         height: '100%',
-        backgroundColor: 'tomato',
       }}
     >
       <PieChart width={800} height={400}>
@@ -25,22 +24,6 @@ const ApplicationsOverview = () => {
           data={data}
           cx={120}
           cy={200}
-          innerRadius={60}
-          outerRadius={80}
-          fill="#8884d8"
-          paddingAngle={5}
-          dataKey="value"
-        >
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-          ))}
-        </Pie>
-        <Pie
-          data={data}
-          cx={420}
-          cy={200}
-          startAngle={180}
-          endAngle={0}
           innerRadius={60}
           outerRadius={80}
           fill="#8884d8"
