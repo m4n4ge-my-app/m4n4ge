de7c4d4dff1890ab9ab7da8fd4c1bf5cf513464a
Dinyar <dinyar.dev@gmail.com>
2024-06-19 23:56:35 -0400

    add legends for charts

--- src/components/widgets/applicationsOverview/ApplicationsOverview.tsx
+++ src/components/widgets/applicationsOverview/ApplicationsOverview.tsx
@@ -1,4 +1,5 @@
 import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip } from 'recharts';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import { applicationSummary } from '../../../utils/mockDataGenerator';
 import { Grid, Typography } from '@mui/material';
 
@@ -6,7 +7,7 @@ const data = [
   { name: 'Applied', value: applicationSummary.Applied },
   { name: 'Engaged', value: applicationSummary.Engaged },
   { name: 'Interviewing', value: applicationSummary.Interviewing },
-  { name: 'Rejected Applicaitons', value: applicationSummary.Rejected },
+  { name: 'Rejections', value: applicationSummary.Rejected },
   { name: 'Offers', value: applicationSummary.Offer },
 ];
 const COLORS = ['lightgray', '#407bff', '#ffc440', '#ff40da', '#40ff64'];
@@ -22,7 +23,7 @@ const ApplicationsOverview = () => {
               data={data}
               cx="35%"
               cy="45%"
-              innerRadius={50}
+              innerRadius={45}
               outerRadius={60}
               fill="#8884d8"
               paddingAngle={5}
@@ -40,7 +41,44 @@ const ApplicationsOverview = () => {
         </ResponsiveContainer>
       </Grid>
       <Grid item xs={4}>
-        <Typography variant="h5">Total: {applicationSummary.total}</Typography>
+        <Typography
+          variant="h6"
+          color="lightgray"
+          style={{ fontWeight: 'bold' }}
+        >
+          Total: {applicationSummary.total}
+        </Typography>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-end',
+            width: '100%',
+            marginTop: '5px',
+            paddingRight: '10px',
+          }}
+        >
+          {data.map((entry, index) => (
+            <div
+              key={index}
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                marginBottom: '10px',
+              }}
+            >
+              <FiberManualRecordIcon
+                style={{
+                  color: COLORS[index % COLORS.length],
+                  marginRight: '5px',
+                  width: '8px',
+                  height: '8px',
+                }}
+              />
+              <span style={{ fontSize: '0.5rem' }}>{entry.name}</span>
+            </div>
+          ))}
+        </div>
       </Grid>
     </Grid>
   );
