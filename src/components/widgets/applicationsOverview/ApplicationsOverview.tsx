8b44f56b5fbb2f5bb4e427fabcffa9de7a728f8f
Dinyar <dinyar.dev@gmail.com>
2024-06-19 22:54:49 -0400

    use ResponsiveContainer form charts to properly postiion the piechart in dashboardgrid

--- src/components/widgets/applicationsOverview/ApplicationsOverview.tsx
+++ src/components/widgets/applicationsOverview/ApplicationsOverview.tsx
@@ -11,19 +11,12 @@ const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 const ApplicationsOverview = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        height: '100%',
-      }}
-    >
-      <PieChart width={800} height={400}>
+    <ResponsiveContainer width="100%" height="100%">
+      <PieChart>
         <Pie
           data={data}
-          cx={120}
-          cy={200}
+          cx="50%"
+          cy="50%"
           innerRadius={60}
           outerRadius={80}
           fill="#8884d8"
@@ -35,7 +28,7 @@ const ApplicationsOverview = () => {
           ))}
         </Pie>
       </PieChart>
-    </div>
+    </ResponsiveContainer>
   );
 };
 
