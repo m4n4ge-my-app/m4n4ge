f2259923730069f2664ac773dc25008f6af00f73
Dinyar <dinyar.dev@gmail.com>
2024-06-22 15:14:47 -0400

    add slice to store toast parameters

---
+++ src/state/feeback/feedbackSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+// Define a type for the slice state
+interface FeedbackState {
+  open: boolean;
+  message: string;
+  severity: 'error' | 'warning' | 'info' | 'success';
+}
+
+// Define the initial state using that type
+const initialState: FeedbackState = {
+  open: false,
+  message: '',
+  severity: 'info',
+};
+
+export const feedbackSlice = createSlice({
+  name: 'feedback',
+  initialState,
+  reducers: {
+    // Action to show the toast with a message and severity
+    show: (
+      state,
+      action: PayloadAction<{
+        message: string;
+        severity: FeedbackState['severity'];
+      }>
+    ) => {
+      state.open = true;
+      state.message = action.payload.message;
+      state.severity = action.payload.severity;
+    },
+    // Action to hide the toast
+    hide: (state) => {
+      state.open = false;
+      state.message = '';
+      state.severity = 'info';
+    },
+  },
+});
+
+// Export actions
+export const { show, hide } = feedbackSlice.actions;
+
+// Export reducer
+export default feedbackSlice.reducer;
