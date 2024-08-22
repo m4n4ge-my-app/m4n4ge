148ed88604a35b6dd89a015a60d91b1ffb6fd8b4
Dinyar <dinyar.dev@gmail.com>
2024-08-22 01:31:03 -0400

    show banner when demo accounts signin; remove obsolete code

--- src/pages/system/layout/Layout.tsx
+++ src/pages/system/layout/Layout.tsx
@@ -6,32 +6,18 @@ import SideBar from '../../../components/navigation/sideBar/SideBar';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../state/store';
 import Navbar from '../../../components/navigation/navbar/Navbar';
-import { useEffect } from 'react';
-import { getLoggedInUser } from '../../../services/auth';
-import { useDispatch } from 'react-redux';
-import { setUser } from '../../../state/user/userSlice';
-import { User } from '../../../state/user/userSlice';
-import { AxiosResponse } from 'axios';
+import React from 'react';
+import { Button, Divider, FormControlLabel, Radio, RadioGroup, Typography } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import InfoIcon from '@mui/icons-material/Info';
 
 const Layout = () => {
-  const dispatch = useDispatch();
+  const [user, setUser] = React.useState('new');
+  const [isBannerVisible, setIsBannerVisible] = React.useState(true)
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await getLoggedInUser();
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const user: User | null = (response as AxiosResponse).data;
-        if (user) {
-          dispatch(setUser(user));
-        }
-      } catch (error) {
-        console.error('Failed to fetch logged in user:', error);
-      }
-    };
-
-    fetchUser().catch((error) => console.error(error));
-  }, [dispatch]);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUser((event.target as HTMLInputElement).value);
+  };
 
   const sidebarWidth = useSelector(
     (state: RootState) => state.sidebar.sidebarWidth
@@ -51,6 +37,57 @@ const Layout = () => {
         }}
       >
         <Toolbar />
+        <Box
+          sx={{
+            p: 3,
+            backgroundColor: isBannerVisible? '#cce5ff' : 'none',
+            display: 'flex',
+            alignItems: 'center',
+            position: 'fixed',
+            top: '0px',
+            width: '100%',
+            height: '150px',
+            zIndex: 1000
+          }}
+        >
+          {isBannerVisible ? (
+            <>
+              <Button
+               onClick={() => {setIsBannerVisible(false)}}
+             >
+               <CloseIcon />
+             </Button>
+             <Divider orientation="vertical" flexItem sx={{ marginRight: '20px', marginLeft: '20px' }} />
+             <Box sx={{ flexGrow: 1 }}>
+               <Typography variant="subtitle1" component="div" sx={{ mb: 1 }}>
+                 You are signed in as a guest with limited access.
+               </Typography>
+               <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                 <span style={{ fontSize: '0.875rem' }}>Explore different user perspectives. Create a personal account for full access.</span>
+                 <RadioGroup
+                   row
+                   aria-labelledby="demo-controlled-radio-buttons-group"
+                   name="controlled-radio-buttons-group"
+                   value={user}
+                   onChange={handleChange}
+                 >
+                   <FormControlLabel value="new" control={<Radio />} label="New User" />
+                   <FormControlLabel value="expert" control={<Radio />} label="Expert User" />
+                 </RadioGroup>
+               </Box>
+             </Box>
+            </>
+          ): (
+            <Button
+               onClick={() => {setIsBannerVisible(true)}}
+             >
+               <InfoIcon 
+                fontSize='large'
+                color="primary"
+              />
+             </Button>
+          )}
+        </Box>
         <Outlet />
       </Box>
     </Box>
