21ad38a89fc744d1d985e416bcd598817da89d8f
Dinyar <dinyar.dev@gmail.com>
2024-08-29 22:43:04 -0400

    remove longer quotes; these push subsquent elements out of its contianer

--- src/components/motivationBar/quotes/sampleQuotes.ts
+++ src/components/motivationBar/quotes/sampleQuotes.ts
@@ -5,18 +5,8 @@ export const quotes = [
       'Perseverance is not a long race; it is many short races one after the other.',
   },
   {
-    author: 'Vince Lombardi',
-    quote:
-      'The difference between a successful person and others is not a lack of strength, not a lack of knowledge, but rather a lack in will.',
-  },
-  {
     author: 'Confucius',
     quote: 'It does not matter how slowly you go as long as you do not stop.',
-  },
-  {
-    author: 'Thomas A. Edison',
-    quote:
-      'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.',
   },
   {
     author: 'Julie Andrews',
@@ -32,25 +22,11 @@ export const quotes = [
       'The only failure is quitting. Everything else is just gathering information.',
   },
   {
-    author: 'Pele',
-    quote:
-      'Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do.',
-  },
-  {
-    author: 'Newt Gingrich',
-    quote:
-      'Perseverance is the hard work you do after you get tired of doing the hard work you already did.',
-  },
-  {
     author: 'Albert Einstein',
     quote:
       'It’s not that I’m so smart, it’s just that I stay with problems longer.',
   },
-  {
-    author: 'Steve Jobs',
-    quote:
-      'I’m convinced that about half of what separates successful entrepreneurs from the non-successful ones is pure perseverance.',
-  },
+
   {
     author: 'Babe Ruth',
     quote: 'It’s hard to beat a person who never gives up.',
@@ -66,11 +42,6 @@ export const quotes = [
   {
     author: 'Winston Churchill',
     quote: 'If you’re going through hell, keep going.',
-  },
-  {
-    author: 'Henry Ford',
-    quote:
-      'When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.',
   },
   {
     author: 'Helen Keller',
