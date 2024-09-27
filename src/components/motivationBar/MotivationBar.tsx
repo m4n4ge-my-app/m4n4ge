88dc936bda71c4cc3bdb5b749e4b7ea390ab87a1
Dinyar <dinyar.dev@gmail.com>
2024-09-27 13:31:26 -0400

    fix motivation bar responsiveness; remove task bages; restructure grid

--- src/components/motivationBar/MotivationBar.tsx
+++ src/components/motivationBar/MotivationBar.tsx
@@ -1,7 +1,5 @@
 //external imports
-import PlaylistAddCheckOutlinedIcon from '@mui/icons-material/PlaylistAddCheckOutlined';
-import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
-import { Badge, Divider, Grid, Typography } from '@mui/material';
+import { Divider, Grid, Typography } from '@mui/material';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-ignore
 import AnalogClock from 'analog-clock-react'; //there is tpescript types for this package from the package maintainer, so this error cant be fixed
@@ -30,8 +28,6 @@ const MotivationBar = () => {
     ` on ${moment(earliestDate.earliestDate).format('ddd, MMM D, YY')}`
   );
   const user = useSelector((state: RootState) => state.user);
-  const [calendarItems, setCalendarItems] = useState(0);
-  const [todoItems, setTodoItems] = useState(0);
 
   const options = {
     useCustomTime: false,
@@ -78,15 +74,6 @@ const MotivationBar = () => {
     // Call the function once immediately because there is discrepency between how long the animation is taking to complete and the interval time
     updateDisplayString();
 
-    //below is random calandar and todo badge numbers showing for expert user. Todo: replace these with actual parameters when these features are developed
-    if (user.user?.email === 'expert_user@m4n4gemy.app') {
-      setCalendarItems(5);
-      setTodoItems(3);
-    } else {
-      setCalendarItems(0);
-      setTodoItems(0);
-    }
-
     const intervalId = setInterval(updateDisplayString, 5000);
 
     return () => clearInterval(intervalId);
@@ -96,167 +83,97 @@ const MotivationBar = () => {
 
   return (
     <Grid
-      container
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: '1rem',
-        height: '250px',
-      }}
-    >
-      <Grid item xs={3} container>
-        <Grid
-          item
-          xs={1}
+    container
+    sx={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      padding: '1rem',
+      height: '100%',
+    }}
+  >
+    <Grid item md={12} lg={3} container>
+      <Grid
+        item
+        xs={1}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        zIndex={1000}
+      >
+        <Typography
+          variant="h5"
           sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
+            transform: 'rotate(-90deg)',
+            transformOrigin: 'center',
+            whiteSpace: 'nowrap',
+            fontWeight: 'bold',
+            color: 'lightgray',
           }}
-          zIndex={1000}
         >
-          <Typography
-            variant="h5"
-            sx={{
-              transform: 'rotate(-90deg)',
-              transformOrigin: 'center',
-              whiteSpace: 'nowrap',
-              fontWeight: 'bold',
-              color: 'lightgray',
-            }}
-          >
-            <Link to="/calendar">
-              <span style={{ color: '#ffc440', textDecoration: 'underline' }}>
-                {day}
-              </span>
-            </Link>{' '}
-            {rest.join(' ')}
-          </Typography>
-        </Grid>
-        <Grid
-          item
-          xs={10}
-          container
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-          sx={{ width: '100%' }}
-        >
-          <AnalogClock {...options} />
-        </Grid>
+          <Link to="/calendar">
+            <span style={{ color: '#ffc440', textDecoration: 'underline' }}>
+              {day}
+            </span>
+          </Link>{' '}
+          {rest.join(' ')}
+        </Typography>
       </Grid>
       <Grid
         item
-        xs={7}
+        xs={10}
         container
         direction="column"
-        justifyContent="space-between"
-        spacing={2}
-        sx={{ height: '100%' }}
-      >
-        <Grid item sx={{width: '100%'}} zIndex={1000}>
-          <Typography
-            variant="body1"
-            align="left"
-            mb={2}
-            sx={{ fontStyle: 'italic', color: 'GrayText' }}
-          >
-            {quote.quote}
-          </Typography>
-          <Typography variant="body1" align="right" mr={1}>
-            {quote.author}
-          </Typography>
-        </Grid>
-        <Box display="flex" justifyContent="center">
-          <Divider sx={{ width: '90%' }} />
-        </Box>
-        <Grid item zIndex={1000}>
-          <Typography
-            variant="h6"
-            sx={{
-              fontWeight: 'bold',
-              color: 'lightgray',
-            }}
-          >
-            Your journey started
-            <span className="animatedDate">{displayString}</span>,{' '}
-            {newUser ? "let's get going" : 'keep pushing'}!
-          </Typography>
-        </Grid>
-      </Grid>
-
-      <Grid
-        item
-        xs={2}
-        container
-        direction="column"
-        justifyContent="space-between"
+        justifyContent="center"
         alignItems="center"
-        sx={{ height: '100%' }}
-        padding={2}
+        sx={{ width: '100%' }}
       >
-        <Box display="flex" alignItems="center" height="100%">
-          <Divider orientation="vertical" sx={{ height: '100%' }} flexItem />
-        </Box>
-        <Grid item>
-          <Typography variant="body1" align="center">
-            Tasks
-          </Typography>
-        </Grid>
-        <Grid
-          item
-          container
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-          spacing={2}
-          sx={{ flex: 1 }}
-          gap={2}
-        >
-          <Grid item>
-            <Link to="/calendar">
-              <Badge
-                badgeContent={calendarItems}
-                color="error"
-                sx={{
-                  '.MuiBadge-badge': {
-                    bgcolor: '#ff40da',
-                    transform: 'scale(0.65)',
-                  },
-                  cursor: 'pointer',
-                  '&:focus': {
-                    outline: 'none',
-                  },
-                }}
-              >
-                <CalendarMonthOutlinedIcon color="action" />
-              </Badge>
-            </Link>
-          </Grid>
-          <Grid item>
-            <Link to="/todos">
-              <Badge
-                badgeContent={todoItems}
-                color="error"
-                sx={{
-                  '.MuiBadge-badge': {
-                    bgcolor: '#ff40da',
-                    transform: 'scale(0.65)',
-                  },
-                  cursor: 'pointer',
-                  '&:focus': {
-                    outline: 'none',
-                  },
-                }}
-              >
-                <PlaylistAddCheckOutlinedIcon color="action" />
-              </Badge>
-            </Link>
-          </Grid>
-        </Grid>
+        <AnalogClock {...options} />
       </Grid>
     </Grid>
+    <Grid
+      item
+      md={12}
+      lg={8}
+      container
+      direction="column"
+      justifyContent="space-between"
+      spacing={2}
+      sx={{ height: '100%' }}
+    >
+      <Grid item sx={{ width: '100%' }} zIndex={1000}>
+        <Typography
+          variant="body1"
+          align="left"
+          mb={2}
+          sx={{ fontStyle: 'italic', color: 'GrayText', marginTop: '10px' }}
+        >
+          {quote.quote}
+        </Typography>
+        <Typography variant="body1" align="right" mr={1}>
+          {quote.author}
+        </Typography>
+      </Grid>
+      <Box display="flex" justifyContent="center">
+        <Divider sx={{ width: '90%', marginTop: '30px' }} />
+      </Box>
+      <Grid item zIndex={1000}>
+        <Typography
+          variant="h6"
+          sx={{
+            fontWeight: 'bold',
+            color: 'lightgray',
+          }}
+        >
+          Your journey started
+          <span className="animatedDate">{displayString}</span>,{' '}
+          {newUser ? "let's get going" : 'keep pushing'}!
+        </Typography>
+      </Grid>
+    </Grid>
+  </Grid>
   );
 };
 
