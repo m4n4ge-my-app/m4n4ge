32e724afc6fa4e9ae2c5256309a37ad1ba855093
Dinyar <dinyar.dev@gmail.com>
2024-09-30 13:04:13 -0400

    manage toggle button state and render appropriate components conditionally

--- src/components/form/forms/ResumesForm.tsx
+++ src/components/form/forms/ResumesForm.tsx
@@ -7,27 +7,37 @@ import {
 } from '@mui/material';
 import FilterDramaIcon from '@mui/icons-material/FilterDrama';
 import EditNoteIcon from '@mui/icons-material/EditNote';
+import { useState } from 'react';
 
 const ResumesForm = () => {
+  const [mode, setMode] = useState('upload');
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
+  const handleChange = (_event: React.MouseEvent<HTMLElement>, newMode: string) => {
+    if (newMode) {
+      setMode(newMode);
+    }
+  };
+
   return (
     <Grid padding={1}>
-      <Grid
-        container
-        item
-        sx={{ justifyContent: 'flex-end' }}
-      >
-        <ToggleButtonGroup aria-label="">
-          <ToggleButton value="" aria-label="">
+      <Grid container item sx={{ justifyContent: 'flex-end' }}>
+        <ToggleButtonGroup
+          value={mode}
+          exclusive
+          onChange={handleChange} //learned that argument passing here is automatically handled by MUI
+          size="small"
+          aria-label="mode selection"
+        >
+          <ToggleButton value="upload" aria-label="upload mode">
             <FilterDramaIcon
               fontSize="small"
               sx={{ marginRight: isMobile ? '0px' : '10px' }}
-            />{' '}
+            />
             {isMobile ? '' : 'Upload'}
           </ToggleButton>
-          <ToggleButton value="" aria-label="">
+          <ToggleButton value="create" aria-label="create mode">
             <EditNoteIcon
               fontSize="small"
               sx={{ marginRight: isMobile ? '0px' : '10px' }}
@@ -37,7 +47,8 @@ const ResumesForm = () => {
         </ToggleButtonGroup>
       </Grid>
       <Grid container item>
-        upload form or create form here
+        {mode === 'upload' && <div>Upload</div>}
+        {mode === 'create' && <div>Create</div>}
       </Grid>
     </Grid>
   );
