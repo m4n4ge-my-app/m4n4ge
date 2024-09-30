51f760902f6152834163e0f85e05fafc69c47e6b
Dinyar <dinyar.dev@gmail.com>
2024-09-30 16:02:31 -0400

    use grid system; use TemplateSelector component and mui textfield component for resume name; add cancel/create buttons

--- src/components/textEditor/TextEditor.tsx
+++ src/components/textEditor/TextEditor.tsx
@@ -1,20 +1,45 @@
-
 import { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import TemplateSelector from './templates/TemplateSelector';
+import { Button, Grid, TextField } from '@mui/material';
 
 const TextEditor = () => {
-    const [value, setValue] = useState('');
-  return (
-    <div style={{ width: '100%', paddingTop: '20px' }}>
-      <ReactQuill
-        theme="snow"
-        value={value}
-        onChange={setValue}
-        style={{ width: '100%', height: '200px' }}
-      />
-    </div>
-  )
-}
+  const [value, setValue] = useState('');
 
-export default TextEditor
+  return (
+    <Grid container spacing={2} style={{ width: '100%', paddingTop: '20px' }}>
+      <Grid item xs={12} container justifyContent="space-between">
+        <TextField
+          id="resume-name"
+          variant="outlined"
+          label="Name"
+          size="small"
+        />
+        <TemplateSelector />
+      </Grid>
+      <Grid item xs={12}>
+        <ReactQuill
+          theme="snow"
+          value={value}
+          onChange={setValue}
+          style={{ width: '100%', height: '1000px' }}
+        />
+      </Grid>
+      <Grid item xs={12} container justifyContent="flex-end" spacing={2}>
+        <Grid item>
+          <Button variant="outlined" color="primary" size="small">
+            Cancel
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" color="primary" size="small">
+            Create
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default TextEditor;
