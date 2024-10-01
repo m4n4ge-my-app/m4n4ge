82a933feed9aa3e9a0cc30019464387f8cdb0bcb
Dinyar <dinyar.dev@gmail.com>
2024-09-30 20:38:58 -0400

    add  a reminder

--- src/components/grids/IManageGrid.tsx
+++ src/components/grids/IManageGrid.tsx
@@ -29,7 +29,8 @@ const IManageGrid = ({ formLabel, listLabel }: Props) => {
         </Grid>
         <Grid item xs={12} sm={12} md={12}>
           <Item className="listBox" sx={{ border: 'none' }}>
-            This is a list container
+            This section should display two lists: one for draft/incomplete resumes and another for complete resumes, each showing their corresponding applications.
+            Additionally, consider using FlatIcon icons to represent different file types, as MUI does not have a built-in icon for .doc file types.
           </Item>
         </Grid>
       </Grid>
