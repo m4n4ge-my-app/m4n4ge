3a42295144b22f0dd760f523e356603a4e536df7
dinyar-dev <dinyar.dev@gmail.com>
2024-12-06 14:24:12 -0500

    add utility function to call get documents endpoint

---
+++ src/services/documents.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosResponse } from 'axios';
+
+const baseUrl = import.meta.env.VITE_BASE_URL as string;
+
+export const getAllDocuments = async (token: string) => {
+  try {
+    const response: AxiosResponse = await axios.get(
+      baseUrl + '/api/documents',
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
