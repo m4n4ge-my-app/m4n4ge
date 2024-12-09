b7ce9b838e32a63f5489f828c6f5e09d645a3297
dinyar-dev <dinyar.dev@gmail.com>
2024-12-09 14:24:24 -0500

    format fix

--- src/components/list/CoverLettersList.tsx
+++ src/components/list/CoverLettersList.tsx
@@ -4,39 +4,25 @@ import { TableCell, TableHead, TableRow } from '@mui/material';
 import DocumentsTable from './listTable/DocumentsTable';
 
 const columns = (
-    <TableHead>
-      <TableRow sx={{ backgroundColor: '#fbfcff' }} id="applications-table-head">
-        <TableCell
-          sx={{ fontWeight: 'bold', color: 'GrayText' }}
-        >
-          <NumbersOutlinedIcon fontSize="inherit" />
-        </TableCell>
-        <TableCell
-          sx={{ fontWeight: 'bold', color: 'GrayText' }}
-        >
-          Name
-        </TableCell>
-        <TableCell
-          sx={{ fontWeight: 'bold', color: 'GrayText' }}
-        >
-          Applications
-        </TableCell>
-        <TableCell
-          sx={{ fontWeight: 'bold', color: 'GrayText' }}
-        >
-          Tags
-        </TableCell>
-        <TableCell align="center" sx={{ fontWeight: 'bold', color: 'GrayText' }}>
-          Actions
-        </TableCell>
-      </TableRow>
-    </TableHead>
-  );
+  <TableHead>
+    <TableRow sx={{ backgroundColor: '#fbfcff' }} id="applications-table-head">
+      <TableCell sx={{ fontWeight: 'bold', color: 'GrayText' }}>
+        <NumbersOutlinedIcon fontSize="inherit" />
+      </TableCell>
+      <TableCell sx={{ fontWeight: 'bold', color: 'GrayText' }}>Name</TableCell>
+      <TableCell sx={{ fontWeight: 'bold', color: 'GrayText' }}>
+        Applications
+      </TableCell>
+      <TableCell sx={{ fontWeight: 'bold', color: 'GrayText' }}>Tags</TableCell>
+      <TableCell align="center" sx={{ fontWeight: 'bold', color: 'GrayText' }}>
+        Actions
+      </TableCell>
+    </TableRow>
+  </TableHead>
+);
 
 const CoverLettersList = () => {
-  return (
-    <DocumentsTable data={[]} columns={columns} />
-  )
-}
+  return <DocumentsTable data={[]} columns={columns} />;
+};
 
-export default CoverLettersList
+export default CoverLettersList;
