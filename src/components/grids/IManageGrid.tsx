fa94c6b8562a52f5cb55c9bf124b14ae977c9a79
dinyar-dev <dinyar.dev@gmail.com>
2024-12-09 14:27:51 -0500

    include other types of documents for preview

--- src/components/grids/IManageGrid.tsx
+++ src/components/grids/IManageGrid.tsx
@@ -70,7 +70,7 @@ const IManageGrid = ({ formLabel, listLabel }: Props) => {
             </Grid>
             <Grid item xs={12} sm={12} md={12}>
               <Item className="listBox" sx={{ border: 'none' }}>
-                {formLabel === 'Resume' && (
+                {(formLabel === 'Resume' || 'cover letter' || 'description') && (
                   <DocumentPreview
                     presignedUrl={focusedDocument.presignedUrl!}
                     title={focusedDocument.name}
